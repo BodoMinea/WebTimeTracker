09ac63884b1e14280e38a677991b1e2635a7c07d
Bodo Minea <bodominea@gmail.com>
2018-08-02 13:37:42 +0300

    Added local storage

--- background.js
+++ background.js
@@ -1,5 +1,45 @@
 nowopen=[], first=true;
 
+chrome.storage.sync.get('sites', function(items) {
+	if(!items.sites){
+		chrome.storage.sync.set({"sites": [] }, function() {
+
+    	});
+	}
+})
+
+function getByHost(url) {
+    for (var i = 0; i < nowopen.length; i++) {
+        if (nowopen[i].host == url) {
+           return i;
+       } 
+    }
+}
+
+function store(host,time){
+	var storage=[],found=false;
+	chrome.storage.sync.get('sites', function(items) {
+		storage = items.sites;
+		for (var i = 0; i < storage.length; i++) {
+	        if (storage[i].host == host) {
+	           found=i;
+	       } 
+	    }
+	    if(found!=false){
+	    	storage[i].times=storage[i].times+1;
+	    	storage[i].milis=storage[i].milis+time;
+	    }else{
+	    	console.log(host);
+	    	console.log({host: host, times: 1, milis: time});
+	    	storage.push({host: host, times: 1, milis: time});
+	    	console.log(storage);
+	    }
+	    chrome.storage.sync.set({"sites": storage }, function() {
+      	
+    	});
+	})
+}
+
 function extractHostname(url) {
     var hostname;
     if (url.indexOf("://") > -1) {
@@ -10,14 +50,6 @@ function extractHostname(url) {
     }
     hostname = hostname.split(':')[0];
     return hostname;
-}
-
-function getByHost(url) {
-    for (var i = 0; i < nowopen.length; i++) {
-        if (nowopen[i].host == url) {
-           return i;
-       } 
-    }
 }
 
 function getByTab(id) {
@@ -39,7 +71,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 		    	last = nowopen[getByTab(tabId)].last;
 		    	nowopen.push({host: extractHostname(tab.url), opened: Date.now(), tab: tabId, last:tab.url});
 		    	nowt = Date.now();
-		    	console.log(nowt-nowopen[getByHost(extractHostname(last))].opened);
+		    	store(nowopen[getByHost(extractHostname(last))].host,nowt-nowopen[getByHost(extractHostname(last))].opened)
 		    	nowopen.splice(getByHost(extractHostname(last)), 1);
 		}
 	}
